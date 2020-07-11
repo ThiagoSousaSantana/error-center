a27daa6864ce7fc5e64e902363a98ce0098c1b5a
Thiago Sousa Santana <thiago91098@gmail.com>
2020-07-11 16:33:17 -0300

    Criado repositories

--- src/main/java/br/com/codenation/ciandt/errorcenter/repositories/ErrorRepository.java
+++ src/main/java/br/com/codenation/ciandt/errorcenter/repositories/ErrorRepository.java
@@ -1,5 +1,6 @@
 package br.com.codenation.ciandt.errorcenter.repositories;
 
+import br.com.codenation.ciandt.errorcenter.models.Error;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
