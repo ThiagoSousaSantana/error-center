2ca57db5031931e4060491199d273d07924cf7d9
Thiago Sousa Santana <thiago91098@gmail.com>
2020-07-11 15:45:48 -0300

    Initial commit

---
+++ src/test/java/br/com/codenation/ciandt/errorcenter/ErrorCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codenation.ciandt.errorcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
